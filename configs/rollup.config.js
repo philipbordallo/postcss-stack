aa5e78037e141410131ed586dcaf47c8377f3b3c
Philip Bordallo <git@philipbordallo.com>
2021-03-06 14:32:08 -0500

    Fix out of date dependencies

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -3,8 +3,8 @@ const babel = require('rollup-plugin-babel');
 module.exports = {
   input: 'src/index.js',
   output: [
-    { file: './dist/index.cjs.js', format: 'cjs' },
-    { file: './dist/index.es.js', format: 'es' },
+    { file: './dist/index.cjs.js', format: 'cjs', exports: 'default' },
+    { file: './dist/index.es.js', format: 'es', exports: 'default' },
   ],
   plugins: [
     babel(),
