87acb20dd09d5c1baa4a8da6b85b1e3c1fc4cb55
Philip Bordallo <git@philipbordallo.com>
2021-09-06 09:40:42 -0400

    Migrate from PostCSS 7 to 8

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -3,8 +3,18 @@ const babel = require('rollup-plugin-babel');
 module.exports = {
   input: 'src/index.js',
   output: [
-    { file: './dist/index.cjs.js', format: 'cjs', exports: 'default' },
-    { file: './dist/index.es.js', format: 'es', exports: 'default' },
+    {
+      file: './dist/index.cjs.js',
+      exports: 'default',
+      footer: 'module.exports.postcss = true',
+      format: 'cjs',
+    },
+    {
+      file: './dist/index.es.js',
+      exports: 'default',
+      footer: 'export const postcss = true',
+      format: 'es',
+    },
   ],
   plugins: [
     babel(),
--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
-import postcss from 'postcss';
 import { parse } from 'postcss-values-parser';
 
-import getStack from './lib/getStack';
+import getStack from './getStack';
 
 const PLUGIN_NAME = 'postcss-stack';
-const stackRegExp = /(^|[^\w-])(stack?)\(/i;
 
-export default postcss.plugin(PLUGIN_NAME, options => (root) => {
-  const stacked = getStack(options);
+export default options => ({
+  postcssPlugin: PLUGIN_NAME,
+  Once(root, { Declaration }) {
+    const stacked = getStack(options);
 
-  root.walkDecls((decl) => {
-    const { value: originalValue } = decl;
+    root.walkDecls((decl) => {
+      const { value } = decl;
 
-    if (stackRegExp.test(originalValue)) {
-      const ast = parse(originalValue);
+      if (/(^|[^\w-])(stack?)\(/i.test(value)) {
+        const ast = parse(value);
 
-      ast.walkFuncs((node) => {
-        const stackQuoted = node.nodes.filter(item => item.type === 'quoted')[0];
+        ast.walkFuncs((node) => {
+          const stackQuoted = node.nodes.filter(item => item.type === 'quoted')[0];
 
-        const quoteRegExp = new RegExp(stackQuoted.quote, 'g');
-        const stack = stackQuoted.value.replace(quoteRegExp, '');
+          const quoteRegExp = new RegExp(stackQuoted.quote, 'g');
+          const stack = stackQuoted.value.replace(quoteRegExp, '');
 
-        if (typeof stacked[stack] !== 'undefined') {
-          // Create a new declaration with just the found stack value
-          const replacement = postcss.decl({
-            prop: '',
-            raws: { between: '' },
-            value: String(stacked[stack]),
-          });
+          if (typeof stacked[stack] !== 'undefined') {
+            // Create a new declaration with just the found stack value
+            const replacement = new Declaration({
+              prop: '',
+              raws: { between: '' },
+              value: String(stacked[stack]),
+            });
 
-          // Replace `stack(STACK_NAME)` func node with new declaration node
-          node.replaceWith(replacement);
+            // Replace `stack(STACK_NAME)` func node with new declaration node
+            node.replaceWith(replacement);
+          }
+          else {
+            throw decl.error('Unknown stack', {
+              plugin: PLUGIN_NAME,
+              word: stack,
+            });
+          }
+        });
+
+        const modifiedValue = ast.toString();
+
+        if (value !== modifiedValue) {
+          decl.value = modifiedValue;
         }
-        else {
-          throw decl.error('Unknown stack', {
-            plugin: PLUGIN_NAME,
-            word: stack,
-          });
-        }
-      });
-
-      const modifiedValue = ast.toString();
-
-      if (originalValue !== modifiedValue) {
-        decl.value = modifiedValue; // eslint-disable-line no-param-reassign
       }
-    }
-  });
+    });
+  },
 });
